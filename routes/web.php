6aea3e8bf4cd4e8495683d3264a938eaac0c32f4
DvianZ <depzyy@yahoo.com>
2026-03-23 16:44:30 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController\TesterController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/test', [TesterController::class, 'index']);
